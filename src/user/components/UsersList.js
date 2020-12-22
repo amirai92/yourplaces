b60d303b279b13c7afbc441254e707db74695e10
amirai92 <34741493+amirai92@users.noreply.github.com>
2020-12-22 13:34:21 +0200

    Added components
    Added User List&User Item Components

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,28 +1,30 @@
-import React from "react";
-import UserItem from "./UserItem";
-import "./UsersList.css";
-const UsersList = (props) => {
+import React from 'react';
+
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No Users found.</h2>
+        <h2>No users found.</h2>
       </div>
     );
   }
+
   return (
-    <ul>
-      {props.items.map((item) => {
-        return (
-          <UserItem
-            key={item.id}
-            id={item.id}
-            image={item.image}
-            name={item.name}
-            placeCount={item.places}
-          />
-        );
-      })}
+    <ul className="users-list">
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
     </ul>
   );
 };
-export default UserList;
+
+export default UsersList;

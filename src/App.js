26ade68d069f9b317d6ec108fa5e24b549afe7ee
amirai92 <34741493+amirai92@users.noreply.github.com>
2020-12-22 23:09:40 +0200

    Update
    Added: google maps, responsive, Navigation, form validation

--- src/App.js
+++ src/App.js
@@ -6,19 +6,27 @@ import {
   Switch,
 } from "react-router-dom";
 import Users from "./user/pages/Users";
+import UserPlaces from "./places/pages/UserPlaces";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Western Wall",
+    description: "Holy place,people from all over the world come to visit",
+    imageUrl:
+      "https://www.itraveljerusalem.com/wp-content/uploads/2016/07/atr-western-wall-2.jpg",
+    address: "Jerusalem",
+    location: {
+      lat: 31.7767189,
+      lng: 35.2366972,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Western Wall",
+    description: "Holy place,people from all over the world come to visit",
+    imageUrl:
+      "https://www.itraveljerusalem.com/wp-content/uploads/2016/07/atr-western-wall-2.jpg",
+    address: "Jerusalem",
+    location: {
+      lat: 31.7767189,
+      lng: 35.2366972,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;

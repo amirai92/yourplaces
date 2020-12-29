2d1806ee66ce01ba2c14f01e93757709e9ed5340
amirai92 <34741493+amirai92@users.noreply.github.com>
2020-12-29 22:19:29 +0200

    Connecting Frontend to the Backend
    Handling CORS errors, sending a login request, error handling in frontend, adding&edit places('POST' Method),show all the users in the frontend('GET' method),updating places('POST' method) and delete place('DELETE' method).

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,41 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 import PlaceList from "../components/PlaceList";
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Western Wall",
-    description: "Holy place,people from all over the world come to visit",
-    imageUrl:
-      "https://www.itraveljerusalem.com/wp-content/uploads/2016/07/atr-western-wall-2.jpg",
-    address: "Jerusalem",
-    location: {
-      lat: 31.7767189,
-      lng: 35.2366972,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Western ",
-    description: "Holy place,people from all over the world come to visit",
-    imageUrl:
-      "https://www.itraveljerusalem.com/wp-content/uploads/2016/07/atr-western-wall-2.jpg",
-    address: "Jerusalem",
-    location: {
-      lat: 31.7767189,
-      lng: 35.2366972,
-    },
-    creator: "u2",
-  },
-];
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const UserPlaces = () => {
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
-  return <PlaceList items={loadedPlaces} />;
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+  const placeDeletedHandler = (deletedPlaceId) => {
+    setLoadedPlaces((prevPlaces) =>
+      prevPlaces.filter((place) => place.id !== deletedPlaceId)
+    );
+  };
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && (
+        <PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler} />
+      )}
+    </>
+  );
 };
 
 export default UserPlaces;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
-const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Amir Aizin",
-      image:
-        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      places: 3,
-    },
-  ];
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
-  return <UsersList items={USERS} />;
+const Users = () => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedUsers, setLoadedUsers] = useState();
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/users"
+        );
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {}
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </>
+  );
 };
 
 export default Users;
